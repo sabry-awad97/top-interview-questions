74cf08b7fd74e9805450ebb0f2ec3c96f0dd6cdf
Sabry Awad <dr.sabry1997@gmail.com>
2023-03-09 13:46:11 +0200

    two sum with space complexity O(1)

--- 1. Two Sum/Rust/src/main.rs
+++ 1. Two Sum/Rust/src/main.rs
@@ -20,11 +20,43 @@ fn two_sum(nums: &[i32], target: i32) -> Option<(usize, usize)> {
     None
 }
 
+fn two_sum_o_1(nums: &[i32], target: i32) -> Option<(usize, usize)> {
+    // if the array is sorted,
+    // we can use a more efficient approach to solve the problem using the two-pointer technique.
+
+    // Set the left pointer to the beginning of the array
+    let mut i = 0;
+    // Set the right pointer to the end of the array
+    let mut j = nums.len() - 1;
+    // Keep looping until the pointers meet in the middle
+    while i < j {
+        // Calculate the sum of the numbers at the two pointers
+        let sum = nums[i] + nums[j];
+        // If the sum is equal to the target, we've found our pair, so return their indices
+        if sum == target {
+            return Some((i, j));
+        // If the sum is less than the target, move the left pointer to the right to increase the sum
+        } else if sum < target {
+            i += 1;
+        // If the sum is greater than the target, move the right pointer to the left to decrease the sum
+        } else {
+            j -= 1;
+        }
+    }
+    // If we reach this point, we've looped through the entire array and haven't found a pair, so return None
+    None
+}
+
 fn main() {
     let nums = vec![1, 2, 3, 4];
     let target = 5;
     let result = two_sum(&nums, target);
     println!("{:?}", result); // Outputs Some((1, 2))
+
+    let nums = vec![1, 2, 3, 4];
+    let target = 5;
+    let result = two_sum_o_1(&nums, target);
+    println!("{:?}", result); // Outputs Some((0, 3))
 }
 
 #[cfg(test)]
@@ -45,6 +77,23 @@ mod tests {
         let target = 4;
         let expected = None;
         let result = two_sum(&nums, target);
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_two_sum() {
+        // Test case 1: Basic example
+        let nums = vec![2, 7, 11, 15];
+        let target = 9;
+        let expected = Some((0, 1));
+        let result = two_sum_o_1(&nums, target);
+        assert_eq!(result, expected);
+
+        // Test case 2: No solution exists
+        let nums = vec![2, 7, 11, 15];
+        let target = 4;
+        let expected = None;
+        let result = two_sum_o_1(&nums, target);
         assert_eq!(result, expected);
     }
 }
